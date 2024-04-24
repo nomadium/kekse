4d42fa51c0386305d134366da85aeccb011ec65a
Miguel Landaeta <miguel@miguel.cc>
2024-04-24 22:56:51 +0100

    Move helper methods to a separate file

--- app/server.rb
+++ app/server.rb
@@ -2,60 +2,7 @@ require "sinatra"
 require "base64"
 require "linzer"
 
-helpers do
-  def pubkey
-    pubkey_material = ENV["PUBKEY"]
-    return nil if pubkey_material.nil?
-    pubkey_material = Base64.strict_decode64(pubkey_material)
-    # XXX: catch exception(s)
-    @pubkey ||= Linzer.new_ed25519_public_key(pubkey_material)
-  end
-
-  def require_authorization?
-    request.path_info != "/hello"
-  end
-
-  def request_headers
-    request
-      .env
-      .select { |(k, _)| k.start_with?("HTTP_") }
-      .transform_keys do |k|
-        k.downcase
-          .gsub(/^http_/, "")
-          .tr("_", "-")
-      end
-  end
-
-  def signature
-    @signature ||= Linzer::Signature.build(request_headers)
-  rescue Linzer::Error => _
-    nil
-  end
-
-  def signed?
-    request_headers.slice("signature", "signature-input").size == 2
-  end
-
-  def valid_signature?
-    return false unless pubkey
-    return false unless signature
-    return false unless signature.components.include?("x-foo")
-
-    message = Linzer::Message.new(request)
-    Linzer.verify(pubkey, message, signature)
-  rescue Linzer::Error => _error
-    # puts error.message # XXX: use a logger for this
-    false
-  end
-
-  def authorized?
-    signed? && valid_signature?
-  end
-
-  def unauthorized?
-    require_authorization? && !authorized?
-  end
-end
+require_relative "helpers"
 
 before do
   halt 401 if unauthorized?
